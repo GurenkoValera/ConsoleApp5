1f67507d137e33bc8517afdd7630f8df82225ba3
valer <valer@DESKTOP-QD94NVR>
2024-10-06 17:30:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод длины катетов
+        Console.Write("Введите длину первого катета: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите длину второго катета: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        // Вычисление гипотенузы по теореме Пифагора
+        double c = Math.Sqrt(a * a + b * b);
+
+        // Вычисление площади треугольника
+        double area = 0.5 * a * b;
+
+        // Вычисление периметра треугольника
+        double perimeter = a + b + c;
+
+        // Вывод результатов
+        Console.WriteLine($"Площадь треугольника: {area:F2} кв. ед.");
+        Console.WriteLine($"Периметр треугольника: {perimeter:F2} ед.");
+    }
+}
